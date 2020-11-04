bdd95ce2a3fb2cf45204297f50813189e79d09d1
OscarAFK <o6trosse@enib.fr>
2020-11-04 14:17:14 +0100

    :construction: :memo: Graph added

---
+++ articles/js/drawChart.js
@@ -0,0 +1,40 @@
+function drawChart(elementId, csvName) {
+	
+	$.get('../csv/'+csvName, function(csvString) {
+	
+		//transform the CSV string into a 2-dimensional array
+		var arrayData = $.csv.toArrays(csvString.trim(), {onParseValue: $.csv.hooks.castToScalar});
+		
+		var arrayDataSimplified = [];
+		
+		for (let i = 0; i < arrayData.length; i++) {
+			if(i%10==0)
+			{
+				arrayDataSimplified.push(arrayData[i]);
+			}
+		}
+		
+		//this new DataTable object holds all the data
+		var data = new google.visualization.arrayToDataTable(arrayDataSimplified);
+
+		var options = {
+			chart: {
+			  title: 'Score of the different movements for the '+elementId+' record',
+			  subtitle: 'from 0 to 1'
+			},
+			width: 900,
+			height: 500,
+			axes: {
+			  x: {
+				0: {side: 'bottom'}
+			  }
+			},
+			enableInteractivity: true,
+			async: true
+		};
+
+		var chart = new google.charts.Line(document.getElementById(elementId));
+
+		chart.draw(data, google.charts.Line.convertOptions(options));
+	});
+}
