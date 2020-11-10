8aab75c10008946d74da3c302115e7b9ba6d0cb6
OscarAFK <o6trosse@enib.fr>
2020-11-10 12:06:28 +0100

    :memo: Doc refactoring

--- articles/js/drawChart.js
+++ articles/js/drawChart.js
@@ -39,3 +39,47 @@ function drawChart(elementId, csvName) {
 		chart.draw(data, google.charts.Line.convertOptions(options));
 	});
 }
+
+function changeYtVideo(elementId, animationAndName){
+	
+	var newSrc = "";
+	
+	switch(animationAndName){
+		case "ChairMael":
+			newSrc+="https://www.youtube.com/embed/Kz9Aw75T9I4";
+			break;
+		case "ChairOscar":
+			newSrc+="https://www.youtube.com/embed/akSPM5uRfX0";
+			break;
+		case "DoorMael":
+			newSrc+="https://www.youtube.com/embed/hiwTt7Tvcq4";
+			break;
+		case "DoorOscar":
+			newSrc+="https://www.youtube.com/embed/unygPkxP10Y";
+			break;
+		case "SalutingMael":
+			newSrc+="https://www.youtube.com/embed/ZXZITcUFRSc";
+			break;
+		case "SalutingOscar":
+			newSrc+="https://www.youtube.com/embed/YRBNX5z3W8s";
+			break;
+		case "SequenceMael":
+			newSrc+="https://www.youtube.com/embed/V41M1reBkdo";
+			break;
+		case "SequenceOscar":
+			newSrc+="https://www.youtube.com/embed/wCnqThEJaRY";
+			break;
+		case "WalkingMael":
+			newSrc+="https://www.youtube.com/embed/XGVayIFEVWg";
+			break;
+		case "WalkingOscar":
+			newSrc+="https://www.youtube.com/embed/i6neqg-fi_E";
+			break;
+		default:
+			console.log("Error : no Youtube video corresponds to your request");
+			break;
+	}
+	
+	
+	document.getElementById(elementId).src = newSrc;
+}
